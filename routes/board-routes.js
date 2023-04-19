2fe5661c399c0b592b107765fbd146effe419f4b
jeongsangtae <wjdtkdxo55@naver.com>
2023-04-20 05:21:13 +0900

    Board NodeJS & MongoDB - count add, post edit add

--- routes/board-routes.js
+++ routes/board-routes.js
@@ -17,7 +17,7 @@ router.get("/posts", async function (req, res) {
     .getDb()
     .collection("posts")
     .find({})
-    .project({ num: 1, title: 1, writer: 1, date: 1 })
+    .project({ num: 1, title: 1, writer: 1, date: 1, count: 1 })
     .toArray();
   res.render("board-list", { posts: posts });
 });
@@ -36,12 +36,14 @@ router.post("/posts", async function (req, res) {
   // num이 존재하지 않으면 1로 초기화
   let num = lastPost ? lastPost.num + 1 : 1;
   let date = new Date();
+  let count = 0;
   const newPost = {
     num: num,
     title: req.body.title,
     writer: req.body.writer,
     password: req.body.password,
     content: req.body.content,
+    count: count,
     date: `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`,
   };
 
@@ -65,7 +67,45 @@ router.get("/posts/:id", async function (req, res) {
     return res.status(404).render("404");
   }
 
+  await db
+    .getDb()
+    .collection("posts")
+    .updateOne({ _id: postId }, { $inc: { count: 1 } });
+
   res.render("post-content", { post: post });
+});
+
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne(
+      { _id: new ObjectId(postId) },
+      { title: 1, writer: 1, password: 1, content: 1 }
+    );
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update-post", { post: post });
+});
+
+router.post("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const updateData = {
+    title: req.body.title,
+    writer: req.body.writer,
+    password: req.body.password,
+    content: req.body.content,
+  };
+
+  await db
+    .getDb()
+    .collection("posts")
+    .updateOne({ _id: new ObjectId(postId) }, { $set: updateData });
+  res.redirect("/posts/" + postId);
 });
 
 module.exports = router;

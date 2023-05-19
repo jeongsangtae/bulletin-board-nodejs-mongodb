71d673628a486dfb9e2c0f2a4c645a64c9a097bf
jeongsangtae <wjdtkdxo55@naver.com>
2023-05-20 05:19:50 +0900

    Board NodeJS & MongoDB - delete confirm modal add

---
+++ public/scripts/delete-confirm-modal.js
@@ -0,0 +1,26 @@
+const deleteForm = document.querySelector("form");
+const confirmModal = document.getElementById("delete-confirm-modal");
+
+const confirmButton = document.getElementById("btn-confirm-delete");
+const cancelButton = document.getElementById("btn-cancel-delete");
+
+const deleteButton = document.getElementById("btn-delete");
+
+function deleteConfirmModal(event) {
+  event.preventDefault();
+  confirmModal.style.display = "block";
+}
+
+deleteButton.addEventListener("click", deleteConfirmModal);
+
+function deleteConfirmModalClose() {
+  confirmModal.style.display = "none";
+}
+
+cancelButton.addEventListener("click", deleteConfirmModalClose);
+
+function deletePostSubmit() {
+  deleteForm.submit();
+}
+
+confirmButton.addEventListener("click", deletePostSubmit);

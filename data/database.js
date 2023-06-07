40d998a8a07e1b89fd201c41109abce05a3b1654
jeongsangtae <wjdtkdxo55@naver.com>
2023-06-07 21:27:42 +0900

    Board NodeJS & MongoDB - deploy error

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-let mongodbUrl = "mongodb://127.0.0.1:27017";
+let mongodbUrl = "mongodb://0.0.0.0:27017";
 
 let database;
 

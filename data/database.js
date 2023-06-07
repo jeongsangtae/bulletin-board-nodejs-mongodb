8c240081533917598f8b8ee1b9ecfc1e39c6a1a6
jeongsangtae <wjdtkdxo55@naver.com>
2023-06-08 01:30:57 +0900

    Board NodeJS & MongoDB - deploy error edit

--- data/database.js
+++ data/database.js
@@ -16,7 +16,8 @@ async function connectToDatabase() {
 
 function getDb() {
   if (!database) {
-    throw { message: "데이터베이스 연결이 설정되지 않았습니다" };
+    // throw { message: "데이터베이스 연결이 설정되지 않았습니다" };
+    throw new Error("데이터베이스 연결이 설정되지 않았습니다");
   }
   return database;
 }

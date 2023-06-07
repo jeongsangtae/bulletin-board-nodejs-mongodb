20bef73ee5d3d8458c64ccebf3c477b2651f30fd
jeongsangtae <wjdtkdxo55@naver.com>
2023-06-08 01:22:22 +0900

    Board NodeJS & MongoDB - deploy error edit

--- data/database.js
+++ data/database.js
@@ -10,12 +10,8 @@ if (process.env.MONGODB_URL) {
 }
 
 async function connectToDatabase() {
-  try {
-    const client = await MongoClient.connect(mongodbUrl);
-    database = client.db("bulletin-board");
-  } catch (error) {
-    console.error("데이터베이스 연결이 설정되지 않았습니다", error);
-  }
+  const client = await MongoClient.connect(mongodbUrl);
+  database = client.db("bulletin-board");
 }
 
 function getDb() {
